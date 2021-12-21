9316b0518073403b9c8f16e011aef41cd5b43d07
Lorenz Ras <lenzras@gmail.com>
2021-12-21 09:49:38 +0900

    Update utils.py

--- registerHankyoToKintone/src/helper/utils.py
+++ registerHankyoToKintone/src/helper/utils.py
@@ -12,11 +12,14 @@ def getGroupIdByMailBox(mailBox):
     return os.getenv('SLACK_CHANNEL_ID_TEST')
 
 def getAppIdByMailBox(mailBox):
+  print("Resolving ID for ", mailBox)
+
   if (mailBox in ["fujisawa@yumetetsu.jp", "toyohashi@yumetetsu.jp"]):
     return os.getenv('KINTONE_APP_ID_FUJISAWA')
   elif (mailBox in ["yawata@yumetetsu.jp", "info@yumetetsu.jp"]):
     return os.getenv('KINTONE_APP_ID_TOYOKAWA')
   else:
+    print("Invalid mailbox, defaulting to TEST.")
     return os.getenv('KINTONE_APP_ID_TEST')
 
 def getAppTokenByMailBox(mailBox):
